19351f33e90e5b677bd4a102d16b5c3a1b7f18f0
Tom Everett <tom@khubla.com>
2018-11-07 18:51:43 -0700

    copywrite

--- src/main/java/com/khubla/telnet/nvt/stream/NVTStream.java
+++ src/main/java/com/khubla/telnet/nvt/stream/NVTStream.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) khubla.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Tom Everett <tom@khubla.com>, 2018
+ */
 package com.khubla.telnet.nvt.stream;
 
 import java.io.Closeable;
